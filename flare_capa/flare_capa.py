dc8f99b381a6513cbafa109764683f863586c93f
william <a876531240@gmail.com>
2023-09-06 10:44:08 +0000

    debugging

--- flare_capa/flare_capa.py
+++ flare_capa/flare_capa.py
@@ -25,7 +25,7 @@ class FlareCapa(ProcessingModule):
 
     def initialize(self):
         if not HAVE_CAPA:
-            raise ModuleInitializationError(self, 'Missing dependency: flare-capa in submodule repo!!')
+            raise ModuleInitializationError(self, 'Missing dependency: flare-capa in submodule repo!!!')
 
     def compute_layout(self, rules, extractor, capabilities):
         """
